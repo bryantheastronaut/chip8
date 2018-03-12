75cfdb1f830708ab5ffc2eb9d7c3a830d439e206
bryantheastronaut <bgilbraith12@gmail.com>
2018-03-11 19:50:47 -0700

    initializing

---
+++ src/chip.rs
@@ -0,0 +1,9 @@
+// Opcodes
+/*
+  0x00E0: clears screen
+  0xDXYN: draws sprite on screen.
+*/
+
+pub fn initialize() {
+
+}
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+// import mod that handles opcode / cpu stuff
+pub mod chip;
+
+fn main() {
+    println!("Hello, world!");
+    // setup graphics, window, size, etc
+    // bind input system
+    // initialize the opcode system
+    // load game
+    chip::testing();
+    'game : loop {
+        'event : loop {
+
+        }
+    }
+
+}
