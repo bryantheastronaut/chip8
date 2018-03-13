a4788e2311742447164eef2d481629a5490e9c3e
bryantheastronaut <bgilbraith12@gmail.com>
2018-03-12 19:21:51 -0700

    adding example for sdl2 bindings

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,55 @@
 /// See http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
 /// for basis of this project
+extern crate sdl2;
 
 
 pub mod chip;
-use sdl2;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
 
 fn main() {
-    println!("Hello, world!");
-    // setup graphics, window, size, etc
-    // bind input system
-    // initialize the opcode system
-    // load game
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
 
-    'game : loop {
-        'event : loop {
-            // watch for events, keypresses, etc then handle it corretly.
+    let window = video_subsystem.window("Rust SDL2 Demo", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().build().unwrap();
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
 
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
         }
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
+
+    // 'game : loop {
+    //     'event : loop {
+    //         match event {
+
+    //         }
+    //         // watch for events, keypresses, etc then handle it corretly.
+
+    //     }
+    // }
 
 }
